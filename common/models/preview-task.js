49589fc1172825f6e590de25f3393ab29c1cc90e
utkarshsharma <utkarsh.hs@gmail.com>
2015-07-10 01:48:12 +0530

    Claim added

--- common/models/preview-task.js
+++ common/models/preview-task.js
@@ -1,3 +1,31 @@
 module.exports = function(PreviewTask) {
+    PreviewTask.claim = function(renderer, cb) {
+    PreviewTask.findOne({where: {renderer: renderer, startTime: null}},
+     function (err, model) {
+          if (err) {
+            console.error(err);
+          }
+          else {
+            if (model != null) {
+              model.updateAttribute('startTime', new Date().getTime() / 1000);
+              // seconds since midnight, 1 Jan 1970
+              model.updateAttribute('expireTime',new Date().getTime()/1000 + 2*1000);
+            }
+            else {
+              console.log (new Error('No tasks pending'));
+            }
+          }
+          cb(err, model);
+   });
+};
 
+
+      PreviewTask.remoteMethod(
+    'claim', 
+    {
+       accepts: {arg: 'renderer', type: 'string'},
+       returns: {arg: 'Preview Task', type: 'object'},
+       http: {path: '/claim', verb: 'get'}
+     }
+);
 };
