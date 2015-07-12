de35982a7e25fe5a8b4b7b0cbd4a2dbc5916fb50
utkarshsharma <utkarsh.hs@gmail.com>
2015-07-12 12:49:45 +0100

    Claim function modified

--- common/models/preview-task.js
+++ common/models/preview-task.js
@@ -1,21 +1,29 @@
 module.exports = function(PreviewTask) {
-    PreviewTask.claim = function(renderer, cb) {
-    PreviewTask.findOne({where: {renderer: renderer, startTime: null}},
+    PreviewTask.claim = function(renderer, cb, next) {
+    PreviewTask.findOne({where: {renderer: renderer,
+                                or: [
+                                  {expireTime: null},
+                                  {expireTime: {lte: Date.now()/1000}}]
+                                }
+
+                        },
      function (err, model) {
-          if (err) {
-            console.error(err);
+        if (err) {
+          console.error(err);
+        }
+        else {
+          if (model != null) {
+              var Task_TTL= 5*60;    //expiration time in seconds
+              // seconds since midnight, 1 Jan 1970
+              model.updateAttribute('startTime', new Date().getTime()/1000);
+              // task expires after the specified TTL
+              model.updateAttribute('expireTime',new Date().getTime()/1000 + Task_TTL);
           }
           else {
-            if (model != null) {
-              model.updateAttribute('startTime', new Date().getTime() / 1000);
-              // seconds since midnight, 1 Jan 1970
-              model.updateAttribute('expireTime',new Date().getTime()/1000 + 2*1000);
-            }
-            else {
-              console.log (new Error('No tasks pending'));
-            }
+            console.log (new Error('No tasks pending'));
           }
-          cb(err, model);
+        }
+        cb(err, model);
    });
 };
 
