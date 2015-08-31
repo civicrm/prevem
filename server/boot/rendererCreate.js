17e55281190d5bcb882346d5d487b5ac1e50973f
Tim Otten <totten@civicrm.org>
2015-08-31 10:39:49 -0700

    rendererCreate.js - Print info to log

--- server/boot/rendererCreate.js
+++ server/boot/rendererCreate.js
@@ -8,16 +8,17 @@ module.exports = function(app) {
   var RoleMapping = app.models.RoleMapping;
   var randomstring = Math.random().toString(36).slice(-8);
 
-  User.create(
-    {email: 'renderers@prevem.prevem', password: randomstring},
-    function(err, Users) {
-      if (err) {
-        throw err;
-      }
-    });
+  var user = {email: 'renderers@prevem.prevem', password: randomstring};
+  User.create(user, function(err, Users) {
+    if (err) {
+      throw err;
+    }
+    console.log('Created user "' + user.email + '" (' + user.password + ')');
+  });
   if (!fs.existsSync('../../node_modules/webmail-renderer/config.json')) {
     fs.createReadStream(path.join(__dirname, '../../node_modules/webmail-renderer/config.json.template'))
       .pipe(fs.createWriteStream(path.join(__dirname, '../../node_modules/webmail-renderer/config.json')));
+    console.log('Created node_modules/webmail-renderer/config.json');
   }
   setTimeout(function() {
     editConfig();
@@ -27,5 +28,6 @@ module.exports = function(app) {
     var config = JSON.parse(fs.readFileSync(path.join(__dirname, '../../node_modules/webmail-renderer/config.json')));
     config.prevemCredentials.password = randomstring;
     fs.writeFileSync(path.join(__dirname, '../../node_modules/webmail-renderer/config.json'), JSON.stringify(config));
+    console.log('Updated node_modules/webmail-renderer/config.json');
   }
 };
